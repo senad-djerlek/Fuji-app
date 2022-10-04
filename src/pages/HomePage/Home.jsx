801e07501b86a93ac455feb0a3b049eead868a36
Amir <amir.ukic123@gmail.com>
2022-10-04 20:25:11 +0200

    anime card and home page

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,7 +1,31 @@
-function Home(){
-    return(<div>
-        <h1>Home</h1>
-    </div>)
+import AnimeCard from "../../components/AnimeCard/AnimeCard";
+import { useState, useEffect } from "react";
+
+function Home() {
+  const [animes, setAnimes] = useState([]);
+
+  async function getAnimes() {
+    const res = await fetch(
+      `https://kitsu.io/api/edge/anime?page[limit]=8&page[offset]=0&filter[text]=naruto`
+    );
+    const data = await res.json();
+    setAnimes(data.data);
+    console.log(data.data);
+  }
+
+  useEffect(() => {
+    getAnimes();
+  }, []);
+  return (
+    <div className="flex flex-wrap ">
+      {animes.map((anime) => (
+        <div key={anime.id}>
+          <AnimeCard image={anime.attributes.posterImage.medium} title={anime.attributes.canonicalTitle} 
+          description={anime.attributes.description} />
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Home;
