d14c2e2148c4910a5b88e6abb9eb481bae9a843e
fariscoric <faris.coric@gmail.com>
2022-10-05 21:05:36 +0200

    Homepage start

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,6 +1,29 @@
+import {useEffect, useState} from 'react'
+import HomePoster from '../../components/HomePoster/HomePoster'
+
 function Home(){
+    
+    const [api, setApi] = useState([])
+
+    const getApi=() => {
+    fetch("https://kitsu.io/api/edge/trending/anime")
+    .then((res) => res.json())
+    .then((json) => {
+        setApi(json.data)
+        console.log(json.data)
+    })
+    }
+    
+    useEffect(() => {
+        getApi()
+},[])
+
     return(<div>
-        <h1>Home</h1>
+        {api.map((el) => (
+            <HomePoster
+            image={el.attributes.coverImage.large}
+            />
+        ))}
     </div>)
 }
 
