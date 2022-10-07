b7c71f115e78ea3f7704d0d61d878f204bb099c1
fariscoric <faris.coric@gmail.com>
2022-10-07 20:28:42 +0200

    Home page slides

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -4,6 +4,8 @@ import HomePoster from '../../components/HomePoster/HomePoster'
 function Home(){
     
     const [api, setApi] = useState([])
+    const [slide, setSlide] = useState(0)
+    const [slide2, setSlide2] = useState(1)
 
     const getApi=() => {
     fetch("https://kitsu.io/api/edge/trending/anime")
@@ -16,14 +18,46 @@ function Home(){
     
     useEffect(() => {
         getApi()
+        
 },[])
 
+// const interval = setInterval(() => {
+//   if (slide>=9) {
+//     setSlide(0)
+//     setSlide2(1)
+//   } else {
+//   setSlide(slide+1)
+//   setSlide2(slide2+1)
+//   console.log(slide, slide2)
+//   }
+// }, 5000);
+
     return(<div>
-        {api.map((el) => (
+        {api.slice(slide,slide2).map((el) => (
             <HomePoster
             image={el.attributes.coverImage.large}
+            title={el.attributes.canonicalTitle}
             />
         ))}
+        <button onClick={() => {
+          if (slide<=1) {
+            setSlide(8)
+            setSlide2(9)
+          } else {
+          setSlide(slide-1)
+          setSlide2(slide2-1)
+          }
+        }}>Prev</button> 
+        
+        <button onClick={() => {
+          if (slide>=9) {
+            setSlide(0)
+            setSlide2(1)
+          } else {
+          setSlide(slide+1)
+          setSlide2(slide2+1)
+          }
+        }}>Next</button>
     </div>)
 }
 
