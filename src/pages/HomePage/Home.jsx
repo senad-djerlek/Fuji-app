6faf590db78b238e6eec144f43f553f777b3f0ca
fariscoric <faris.coric@gmail.com>
2022-10-12 17:37:01 +0200

    Carousel

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -2,25 +2,36 @@ import {useEffect, useState} from 'react'
 import HomePoster from '../../components/HomePoster/HomePoster'
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import HomeCategories from '../../components/HomeCategories/HomeCategories'
 
 
 
 function Home(){
     
-    const [api, setApi] = useState([])
+    const [trending, setTrending] = useState([])
+    const [categories, setCategories] = useState([])
 
-    const getApi=() => {
+    const getTrending=() => {
     fetch("https://kitsu.io/api/edge/trending/anime")
     .then((res) => res.json())
     .then((json) => {
-        setApi(json.data)
+        setTrending(json.data)
         console.log(json.data)
     })
     }
+
+    const getCategories=() => {
+      fetch("https://kitsu.io/api/edge/categories")
+      .then((res) => res.json())
+      .then((json) => {
+          setCategories(json.data)
+          console.log(json.data)
+      })
+      }
     
     useEffect(() => {
-        getApi()
-        
+        getTrending();
+        getCategories();
 },[])
 
     return(<div>
@@ -29,13 +40,24 @@ function Home(){
         emulateTouch="true"
         autoPlay="true"
         interval={5000}>
-          {api.slice(0,5).map((el) => (
+          {trending.slice(0,5).map((el) => (
             <HomePoster
             image={el.attributes.coverImage.large}
             title={el.attributes.canonicalTitle}
             />
           ))}
         </Carousel>
+        
+          {/* {categories.map((el) => (
+            <div>
+              <h1>{el.attributes.title}</h1>
+              <Carousel>
+                {el.relationships.anime.links.related}
+              </Carousel>
+            </div>
+
+          ))} */}
+        
     </div>)
 }
 
