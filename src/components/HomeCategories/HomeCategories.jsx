37e9e5db08a8190ab5dda0af61b3c0bdeb549eae
fariscoric <faris.coric@gmail.com>
2022-10-16 17:23:56 +0200

    home categories

--- src/components/HomeCategories/HomeCategories.jsx
+++ src/components/HomeCategories/HomeCategories.jsx
@@ -1,5 +1,5 @@
 export default function HomeRows({image, title}) {
     return (
-            <img src={image} className="w-full mr-5 hover:scale-110 object-contain h-52 transition-transform duration-400"/>
+            <img src={image} className="w-full shadow-xl mr-5 hover:scale-110 object-contain h-52 transition-transform duration-400"/>
     )
 }
--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -4,7 +4,7 @@ import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import HomeRows from '../../components/HomeCategories/HomeCategories';
 import './Home.css'
-
+import { useNavigate } from 'react-router-dom'
 
 
 function Home(){
@@ -14,6 +14,8 @@ function Home(){
     const [romance, setRomance] = useState([])
     const [horror, setHorror] = useState([])
     const [action, setAction] = useState([])
+
+    const navigate = useNavigate()
 
     const getTrending=() => {
     fetch("https://kitsu.io/api/edge/trending/anime")
@@ -68,8 +70,9 @@ function Home(){
         getAction();
 },[])
 
-    return(<div>
+    return(<div className='bg-dark'>
         <Carousel
+        className='shadow-xl'
         infiniteLoop="true"
         emulateTouch="true"
         autoPlay="true"
@@ -81,33 +84,43 @@ function Home(){
             />
           ))}
         </Carousel>
-            <h1 className='flex flex-start text-2xl ml-5'>Adventure</h1>
-          <div className='rowPosters flex flex-row overflow-y-hidden overflow-x-scroll p-5'>
+            <h1 className='flex flex-start text-2xl ml-5 mt-5 text-white'>Adventure</h1>
+          <div className='rowPosters flex flex-row overflow-y-hidden overflow-x-scroll p-5 m-3'>
           {categories.map((el) => (
             <HomeRows
+            onClick={() => {
+              navigate(`anime/${el.id}/${el.attributes.canonicalTitle}`, {
+                state: {
+                  id: el.id,
+                  image: el.attributes.posterImage.small,
+                  title: el.attributes.canonicalTitle,
+                  description: el.attributes.description
+                }
+            })
+            }}
             image={el.attributes.posterImage.small}
             />
           ))}
           </div>
 
-          <h1 className='flex flex-start text-2xl ml-5'>Romance</h1>
-          <div className='rowPosters flex flex-row overflow-y-hidden overflow-x-scroll p-5'>
+          <h1 className='flex flex-start text-2xl ml-5 mt-5 text-white'>Romance</h1>
+          <div className='rowPosters flex flex-row overflow-y-hidden overflow-x-scroll p-5 m-3'>
           {romance.map((el) => (
             <HomeRows
             image={el.attributes.posterImage.small}
             />
           ))}
           </div>
-          <h1 className='flex flex-start text-2xl ml-5'>Horror</h1>
-          <div className='rowPosters2 flex flex-row overflow-y-hidden overflow-x-scroll p-5'>
+          <h1 className='flex flex-start text-2xl ml-5 mt-5 text-white'>Horror</h1>
+          <div className='rowPosters flex flex-row overflow-y-hidden overflow-x-scroll p-5 m-3'>
           {horror.map((el) => (
             <HomeRows
             image={el.attributes.posterImage.small}
             />
           ))}
           </div>
-          <h1 className='flex flex-start text-2xl ml-5'>Action</h1>
-          <div className='rowPosters2 flex flex-row overflow-y-hidden overflow-x-scroll p-5'>
+          <h1 className='flex flex-start text-2xl ml-5 mt-5 text-white'>Action</h1>
+          <div className='rowPosters flex flex-row overflow-y-hidden overflow-x-scroll p-5 ml-3'>
           {action.map((el) => (
             <HomeRows
             image={el.attributes.posterImage.small}
